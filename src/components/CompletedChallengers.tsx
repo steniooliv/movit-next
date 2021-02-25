4ef5d24b6dcbb8b7a9e174b6e12e320887fb4ecb
Stenio <steniooliv@gmail.com>
2021-02-25 00:20:33 -0300

    create completed challengers component

---
+++ src/components/CompletedChallengers.tsx
@@ -0,0 +1,12 @@
+import styles from '../styles/components/CompletedChallengers.module.css';
+
+export function CompletedChallengers() {
+  return (
+
+    <div className={styles.CompletedChallengersContainer}>
+      <span>Desafios Completos</span>
+      <span>5</span>
+    </div>
+
+  );
+}
